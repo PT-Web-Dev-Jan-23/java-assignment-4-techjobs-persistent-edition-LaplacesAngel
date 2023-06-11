eedb86d619af743bdd8f6988b1d5e3c616f88071
paul <paulentz@gmail.com>
2023-06-10 22:36:09 -0500

    almost done, have a failing test

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,13 +1,21 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
@@ -15,11 +23,20 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+
     @RequestMapping("")
     public String index(Model model) {
-
         model.addAttribute("title", "My Jobs");
-
+        model.addAttribute("jobs",jobRepository.findAll());
         return "index";
     }
 
@@ -27,8 +44,11 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
+        model.addAttribute("employers",employerRepository.findAll());
+        model.addAttribute("skills",skillRepository.findAll());
         return "add";
     }
+
 
     @PostMapping("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
@@ -36,16 +56,42 @@ public class HomeController {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
-            return "add";
+            return "/add";
         }
+
+
+        List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+        if(skillObjs.isEmpty()){
+            model.addAttribute("title","Invalid skill id");
+        } else {
+            newJob.setSkills(skillObjs);
+            jobRepository.save(newJob);
+        }
+
+        Optional<Employer> result = employerRepository.findById(employerId);
+        if(result.isEmpty()){
+            model.addAttribute("title","Invalid employer id");
+        } else {
+            Employer employer = result.get();
+            newJob.setEmployer(employer);
+            jobRepository.save(newJob);
+        }
+
 
         return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
-
-        return "view";
+        Optional optionalJob = jobRepository.findById(jobId);
+        if(optionalJob.isPresent()){
+            Job job = (Job) optionalJob.get();
+            model.addAttribute("job", job);
+            return "view";
+        }
+        else {
+            return "redirect:../";
+        }
     }
 
 
